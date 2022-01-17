0da3516471d2b64784308cbdf4a93ac6a3ac508e
XueweiYan <xueweiyan0405@gmail.com>
2022-01-16 16:38:10 -0800

    xuewei/v1.0

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Most common English words: https://github.com/first20hours/google-10000-english
 
 ### Updates:
 
+##### 2022.1.9 First Demo
+
+- Preprocessed word list data and their vector embeddings.
+
+- implmented a tiny demo that can have the AI generate hints and guesses through notebooks. The main goal is to test the use of different packages and find the optimal way to develop OOP structure for later versions.
+
 ##### 2022.1.15 Version 0.1
 
 - Only support full AI automated game play, human actions to be implemented.
@@ -17,3 +23,8 @@ Most common English words: https://github.com/first20hours/google-10000-english
 
 - Implemented a conservative index (ranged 0~1, with 1 being the most conservative spymaster, now set to 0.7). This index represents the potential risk the spymaster is willing to take in exchange for guessing more words in a single round.
 
+##### 2022.1.16 Version 1.0
+
+- Redo the structure of the code base, introduced Word and Word Base object for better translation between human and AI.
+
+- Full functions of the game implemented, now the game can be played through `cd code`, then `python codenames.py`. Now, we only have one set of strategy and one set of vector embeddings, we will focus on getting more options and test for the optimal for later updates.
--- code/codenames.py
+++ code/codenames.py
@@ -1,83 +1,73 @@
 import numpy as np
 import json
-from termcolor import colored
+from termcolor import colored, cprint
 from player import Player
+from wordBase import WordBase, Word
 import time
+import os
 
 class Codenames:
     def __init__(self,
-                team_a_spymaster,
-                team_a_guesser,
-                team_b_spymaster,
-                team_b_guesser,
+                team_a_ms, team_a_gs, team_b_ms, team_b_gs,
                 codenames_file='../data/processed_data/codenames_vecs.json',
                 dictionary_file='../data/processed_data/dictionary_vecs.json',
                 threshold=0.1
     ):
     
-        self.codenames_words, self.codenames_vecs = self.load_data(codenames_file)
-        self.dictionary_words, self.dictionary_vecs = self.load_data(dictionary_file)
+        self.word_base = WordBase(codenames_file, dictionary_file)
         self.threshold = threshold
         self.initiate_game()
-        self.initiate_players(team_a_spymaster, team_a_guesser, team_b_spymaster, team_b_guesser)
-
-    def load_data(self, file):
-        with open(file) as f:
-            content = json.load(f)
-        words = np.array(list(content.keys()))
-        vecs = np.array(list(content.values()))
-        return words, vecs
+        self.initiate_players(team_a_ms, team_a_gs, team_b_ms, team_b_gs)
 
     def initiate_game(self):
         # Randomly choose 25 words and assign to teams (9 -> red,8 -> blue,7 -> neutral,1 -> assassin)
-        self.game_words = np.random.choice(range(len(self.codenames_words)), 25, replace=False)
+        self.game_words = np.random.choice(self.word_base.get_codenames_words(), 25, replace=False)
         self.guess_status = np.zeros(25)
+        self.word_team = [1] * 9 + [2] * 8 + [3] * 7 + [4]
         # Resuffle for random display
         self.display_order = np.random.choice(range(25), 25, replace=False)
-        # Compute cosine similarity matrix, shape = (NUM_DICT, NUM_CODENAMES)
-        self.cosine_sim_mat = np.matmul(self.dictionary_vecs, self.codenames_vecs.T)
-        # Make a mapping of codenames words id to dictionary words id
-        self.mapping_dict = dict()
-        for idx in self.game_words:
-            match_idx = np.where(self.dictionary_words==self.codenames_words[idx])[0]
-            if len(match_idx) > 0:
-                self.mapping_dict[idx] = match_idx[0]
-            else:
-                self.mapping_dict[idx] = None
         return None
     
-    def initiate_players(self, team_a_spymaster, team_a_guesser, team_b_spymaster, team_b_guesser):
-        self.ta_ms = Player(team_a_spymaster, self.game_words, self.guess_status, self.cosine_sim_mat, self.mapping_dict, team='a')
-        self.ta_gs = Player(team_a_guesser, self.game_words, self.guess_status, self.cosine_sim_mat, self.mapping_dict, team='a')
-        self.tb_ms = Player(team_b_spymaster, self.game_words, self.guess_status, self.cosine_sim_mat, self.mapping_dict, team='b')
-        self.tb_gs = Player(team_b_guesser, self.game_words, self.guess_status, self.cosine_sim_mat, self.mapping_dict, team='b')
-        self.all_players = [self.ta_ms, self.ta_gs, self.tb_ms, self.tb_gs]
+    def initiate_players(self, team_a_ms, team_a_gs, team_b_ms, team_b_gs):
+        self.ta_ms = Player(team_a_ms, self.word_base, self.game_words, self.guess_status, team='a', role='spymaster')
+        self.ta_gs = Player(team_a_gs, self.word_base, self.game_words, self.guess_status, team='a', role='guesser')
+        self.tb_ms = Player(team_b_ms, self.word_base, self.game_words, self.guess_status, team='b', role='spymaster')
+        self.tb_gs = Player(team_b_gs, self.word_base, self.game_words, self.guess_status, team='b', role='guesser')
         return None
         
-    def display_board(self):
-        color_ref = {0: 'yellow', 1: 'red', 2: 'blue', 3: 'white', 4: 'black'}
+    def display_board(self, status_ref, team, turn):
+        color = 'red' if team == 'A' else 'cyan'
+        os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+        cprint("*" * 85, color)
+        cprint("*" + " " * 35 + "TEAM {} TURN {}".format(team, turn) + " " * 35 + "*", color)
+        cprint("*" * 85, color)
+        color_ref = {0: 'yellow', 1: 'red', 2: 'cyan', 3: 'white', 4: 'magenta'}
         for i in range(5):
             for j in range(5):
-                word = self.codenames_words[self.game_words[self.display_order[i*5+j]]]
-                color = color_ref[self.guess_status[self.display_order[i*5+j]]]
-                print(colored(word, color).center(40), end='')
+                word = self.game_words[self.display_order[i*5+j]].word
+                color = color_ref[status_ref[self.display_order[i*5+j]]]
+                if self.guess_status[self.display_order[i*5+j]] != 0:
+                    cprint(word.center(17), color, 'on_{}'.format(color), end='')
+                else:
+                    cprint(word.center(17), color, end='')
             print("\n")
         return None
         
     def play(self):
+        turn = 1
         while True:
-            print("*" * 145)
-            print("*" + " " * 65 + "TEAM A's turn" + " " * 65 + "*")
-            print("*" * 145)
-            self.display_board()
+                    
+            self.display_board(self.word_team, 'A', turn)
+            word, count = self.ta_ms.give_hint()
             time.sleep(5)
-            word_id, word_count = self.ta_ms.give_hint()
-            print("TEAM A Spymaster: My hint is {}: {}".format(self.dictionary_words[word_id], word_count))
-            guess = self.ta_gs.make_guess(word_id, word_count)
-            for idx in guess:
+            
+            self.display_board(self.guess_status, 'A', turn)
+            print("TEAM A Spymaster: My hint is {}: {}".format(word, count))
+            guess = self.ta_gs.make_guess(word, count)
+            for word in guess:
                 time.sleep(3)
-                print("TEAM A Guesser: I guess \"{}\" is our word".format(self.codenames_words[idx]))
-                idx_in_game_words = np.argwhere(self.game_words == idx)
+                print("TEAM A Guesser: I guess \"{}\" is our word".format(word))
+                idx_in_game_words = np.argwhere(self.game_words == word)
                 time.sleep(1.5)
                 if idx_in_game_words < 9:
                     print("TEAM A Spymaster: That is correct!")
@@ -95,25 +85,27 @@ class Codenames:
                     self.guess_status[idx_in_game_words] = 4
                     break
             if 0 not in self.guess_status[0:9]:
-                print("GAME OVER! TEAM A wins this one by finding out all their words!")
+                self.display_board(self.guess_status, 'A', turn)
+                print("GAME OVER IN {} TURNS! TEAM A wins this one by finding out all their words!".format(turn))
                 break
             if self.guess_status[-1] != 0:
-                print("GAME OVER! TEAM B wins this one as TEAM A revealed the assassin word!")
+                self.display_board(self.guess_status, 'A', turn)
+                print("GAME OVER IN {} TURNS! TEAM B wins this one as TEAM A revealed the assassin word!".format(turn))
                 break
             print("END OF TURN")
             time.sleep(2)
-            print("*" * 145)
-            print("*" + " " * 65 + "TEAM B's turn" + " " * 65 + "*")
-            print("*" * 145)
-            self.display_board()
+            
+            self.display_board(self.word_team, 'B', turn)
+            word, count = self.tb_ms.give_hint()
             time.sleep(5)
-            word_id, word_count = self.tb_ms.give_hint()
-            print("TEAM B Spymaster: My hint is {}: {}".format(self.dictionary_words[word_id], word_count))
-            guess = self.tb_gs.make_guess(word_id, word_count)
-            for idx in guess:
+            
+            self.display_board(self.guess_status, 'B', turn)
+            print("TEAM B Spymaster: My hint is {}: {}".format(word, count))
+            guess = self.tb_gs.make_guess(word, count)
+            for word in guess:
                 time.sleep(3)
-                print("TEAM B Guesser: I guess \"{}\" is our word".format(self.codenames_words[idx]))
-                idx_in_game_words = np.argwhere(self.game_words == idx)
+                print("TEAM B Guesser: I guess \"{}\" is our word".format(word))
+                idx_in_game_words = np.argwhere(self.game_words == word)
                 time.sleep(1.5)
                 if idx_in_game_words < 9:
                     print("TEAM B Spymaster: That is incorrect... It is the opponents' word...")
@@ -131,26 +123,25 @@ class Codenames:
                     self.guess_status[idx_in_game_words] = 4
                     break
             if 0 not in self.guess_status[9:17]:
-                print("GAME OVER! TEAM B wins this one by finding out all their words!")
+                self.display_board(self.guess_status, 'B', turn)
+                print("GAME OVER IN {} TURNS! TEAM B wins this one by finding out all their words!".format(turn))
                 break
             if self.guess_status[-1] != 0:
-                print("GAME OVER! TEAM A wins this one as TEAM A revealed the assassin word!")
+                self.display_board(self.guess_status, 'B', turn)
+                print("GAME OVER IN {} TURNS! TEAM A wins this one as TEAM A revealed the assassin word!".format(turn))
                 break
             print("END OF TURN")
             time.sleep(2)
+            
+            turn += 1
 
 def __main__():
-    team_a_spymaster = input("Choose the player for TEAM A Spymaster [1. Human / 2. AI]:  ")
-    team_a_guesser = input("Choose the player for TEAM A Guesser [1. Human / 2. AI]:  ")
-    team_b_spymaster = input("Choose the player for TEAM B Spymaster [1. Human / 2. AI]:  ")
-    team_b_guesser = input("Choose the player for TEAM B Guesser [1. Human / 2. AI]:  ")
+    ta_ms = input("Choose the player for TEAM A Spymaster [1. Human / 2. AI]:  ")
+    ta_gs = input("Choose the player for TEAM A Guesser [1. Human / 2. AI]:  ")
+    tb_ms = input("Choose the player for TEAM B Spymaster [1. Human / 2. AI]:  ")
+    tb_gs = input("Choose the player for TEAM B Guesser [1. Human / 2. AI]:  ")
     input_encoding = {'1': 'human', '2': 'ai'}
-    game = Codenames(
-        input_encoding[team_a_spymaster],
-        input_encoding[team_a_guesser],
-        input_encoding[team_b_spymaster],
-        input_encoding[team_b_guesser]
-    )
+    game = Codenames(input_encoding[ta_ms], input_encoding[tb_gs], input_encoding[ta_ms], input_encoding[tb_gs])
     game.play()
 
 if __name__ == '__main__':
