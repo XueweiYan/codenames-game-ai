6bb54874a9dad476f6e26adcbe09ce7e26da59f7
YongqingLi14 <62914852+YongqingLi14@users.noreply.github.com>
2022-01-24 08:46:12 -0800

    dict w 3k and 5k in size

--- data_processing/DBLP.py
+++ data_processing/DBLP.py
@@ -13,16 +13,12 @@ with open(codenames_raw) as f:
 
 codenames_word = set([x.strip().lower() for x in raw_text])
 
-
+dict_size = [3000, 5000, 10000, 20000, 30000]
+dict_word = {}
 # Process dictionary corpus and separate by size
-with open(dictionary_raw) as f:
-    word_10k = [next(f) for x in range(10000)]
-    word_20k = [next(f) for x in range(10000)]
-    word_30k = [next(f) for x in range(10000)]
-
-dict_word_10k = set([x.strip().split()[0].lower() for x in word_10k])
-dict_word_20k = set([x.strip().split()[0].lower() for x in word_20k])
-dict_word_30k = set([x.strip().split()[0].lower() for x in word_30k])
+for size in dict_size:
+    with open(dictionary_raw) as f:
+        dict_word[size] = set([next(f).strip().split()[0].lower() for x in range(size)])
 
 
 #process text corpus to train word2vec model
@@ -41,42 +37,38 @@ def parseData(filename):
             data.append(words)
     return data
 
-#parse document
-data = parseData('../data/raw_data/DBLP.txt')
+# #parse document
+# data = parseData('../data/raw_data/DBLP.txt')
 
-#train word2vec model
-model = Word2Vec(data, vector_size=300, window=5, min_count=1, workers=4)
+# #train word2vec model
+# model = Word2Vec(data, vector_size=300, window=5, min_count=1, workers=4)
 
-#populate key-value pair (word:vector embeding)
-codenames_vecs = {}
-dictionary_vecs_10k = {}
-dictionary_vecs_20k = {}
-dictionary_vecs_30k = {}
+# #populate key-value pair (word:vector embeding)
+# codenames_vecs = {}
+# dictionary_vecs = {}
 
-for i in model.wv.key_to_index:
-    if i in codenames_word:
-        codenames_vecs[i] = model.wv[i].tolist()
-    elif i in dict_word_10k:
-        dictionary_vecs_10k[i] = model.wv[i].tolist()
-    elif i in dict_word_20k:
-        dictionary_vecs_20k[i] = model.wv[i].tolist()
-    elif i in dict_word_30k:
-        dictionary_vecs_30k[i] = model.wv[i].tolist()
+# for i in model.wv.key_to_index:
+#     if i in codenames_word:
+#         codenames_vecs[i] = model.wv[i].tolist()
+#     if i in dict_word[3000]:
+#         dictionary_vecs[3][i] = model.wv[i].tolist()
+#     if i in dict_word[5000]:
+#         dictionary_vecs[5][i] = model.wv[i].tolist()
+#     if i in dict_word[10000]:
+#         dictionary_vecs[10][i] = model.wv[i].tolist()
+#     if i in dict_word[20000]:
+#         dictionary_vecs[20][i] = model.wv[i].tolist()
+#     if i in dict_word[30000]:
+#         dictionary_vecs[30][i] = model.wv[i].tolist()
 
-codenames_output = '../data/processed_data/codenames_vecs_dblp.json'
-dictionary_output_10k = '../data/processed_data/dictionary_vecs_dblp_10k.json'
-dictionary_output_20k = '../data/processed_data/dictionary_vecs_dblp_20k.json'
-dictionary_output_30k = '../data/processed_data/dictionary_vecs_dblp_30k.json'
-dict_files = [dictionary_output_10k, dictionary_output_20k, dictionary_output_30k]
-dict_vecs = [dictionary_vecs_10k, 
-            {**dictionary_vecs_10k, **dictionary_vecs_20k},
-            {**dictionary_vecs_10k, **dictionary_vecs_20k, **dictionary_vecs_30k}]
+# codenames_output = '../data/processed_data/codenames_vecs_dblp.json'
+# dict_output = [3,5,10,20,30]
 
 
-#output word vectors as key value pair in json
-with open(codenames_output, 'w') as f:
-    json.dump(codenames_vecs, f)
+# #output word vectors as key value pair in json
+# with open(codenames_output, 'w') as f:
+#     json.dump(codenames_vecs, f)
 
-for i in range(3):
-    with open(dict_files[i], 'w') as f:
-        json.dump(dict_vecs[i], f)
+# for i in dict_output:
+#     with open('../data/processed_data/dictionary_vecs_dblp_' + str(i) + 'k.json', 'w') as f:
+#         json.dump(dictionary_vecs[i], f)
