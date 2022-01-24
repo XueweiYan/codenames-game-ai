d1f3809752c192f4fdc8c623e057ce4a457b3963
YongqingLi14 <62914852+YongqingLi14@users.noreply.github.com>
2022-01-24 08:51:18 -0800

    uncomment lines

--- data_processing/DBLP.py
+++ data_processing/DBLP.py
@@ -37,38 +37,38 @@ def parseData(filename):
             data.append(words)
     return data
 
-# #parse document
-# data = parseData('../data/raw_data/DBLP.txt')
+#parse document
+data = parseData('../data/raw_data/DBLP.txt')
 
-# #train word2vec model
-# model = Word2Vec(data, vector_size=300, window=5, min_count=1, workers=4)
+#train word2vec model
+model = Word2Vec(data, vector_size=300, window=5, min_count=1, workers=4)
 
-# #populate key-value pair (word:vector embeding)
-# codenames_vecs = {}
-# dictionary_vecs = {}
+#populate key-value pair (word:vector embeding)
+codenames_vecs = {}
+dictionary_vecs = {}
 
-# for i in model.wv.key_to_index:
-#     if i in codenames_word:
-#         codenames_vecs[i] = model.wv[i].tolist()
-#     if i in dict_word[3000]:
-#         dictionary_vecs[3][i] = model.wv[i].tolist()
-#     if i in dict_word[5000]:
-#         dictionary_vecs[5][i] = model.wv[i].tolist()
-#     if i in dict_word[10000]:
-#         dictionary_vecs[10][i] = model.wv[i].tolist()
-#     if i in dict_word[20000]:
-#         dictionary_vecs[20][i] = model.wv[i].tolist()
-#     if i in dict_word[30000]:
-#         dictionary_vecs[30][i] = model.wv[i].tolist()
+for i in model.wv.key_to_index:
+    if i in codenames_word:
+        codenames_vecs[i] = model.wv[i].tolist()
+    if i in dict_word[3000]:
+        dictionary_vecs[3][i] = model.wv[i].tolist()
+    if i in dict_word[5000]:
+        dictionary_vecs[5][i] = model.wv[i].tolist()
+    if i in dict_word[10000]:
+        dictionary_vecs[10][i] = model.wv[i].tolist()
+    if i in dict_word[20000]:
+        dictionary_vecs[20][i] = model.wv[i].tolist()
+    if i in dict_word[30000]:
+        dictionary_vecs[30][i] = model.wv[i].tolist()
 
-# codenames_output = '../data/processed_data/codenames_vecs_dblp.json'
-# dict_output = [3,5,10,20,30]
+codenames_output = '../data/processed_data/codenames_vecs_dblp.json'
+dict_output = [3,5,10,20,30]
 
 
-# #output word vectors as key value pair in json
-# with open(codenames_output, 'w') as f:
-#     json.dump(codenames_vecs, f)
+#output word vectors as key value pair in json
+with open(codenames_output, 'w') as f:
+    json.dump(codenames_vecs, f)
 
-# for i in dict_output:
-#     with open('../data/processed_data/dictionary_vecs_dblp_' + str(i) + 'k.json', 'w') as f:
-#         json.dump(dictionary_vecs[i], f)
+for i in dict_output:
+    with open('../data/processed_data/dictionary_vecs_dblp_' + str(i) + 'k.json', 'w') as f:
+        json.dump(dictionary_vecs[i], f)
