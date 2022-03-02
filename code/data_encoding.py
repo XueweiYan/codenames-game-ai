6604072fa77405bd033ac522ec7d906bdf906744
YongqingLi14 <62914852+YongqingLi14@users.noreply.github.com>
2022-03-01 17:23:00 -0800

    use final 3 datasets

--- code/data_encoding.py
+++ code/data_encoding.py
@@ -1,7 +1,7 @@
 import os
 
 mypath = '../../AI_dataset' #file that stores all data, should be in same directory as git repo
-file = [os.path.join(mypath, f) for f in os.listdir(mypath) if not f.startswith('.')] #gets name of all 33 datafile, ignores .DS_Store file
-key = range(1, 34)
+file = [os.path.join(mypath, f) for f in os.listdir(mypath) if not f.startswith('.')] #gets name of all 3 datafile, ignores .DS_Store file
+key = range(1, 4)
 
 data_encodings = dict(zip(key, file))
