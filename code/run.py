aa942a280329b372757c42837da4f6b7f0eea04e
YongqingLi14 <62914852+YongqingLi14@users.noreply.github.com>
2022-03-07 15:47:58 -0800

    change dataset names

--- code/run.py
+++ code/run.py
@@ -13,7 +13,7 @@ players = " ".join([ta_ms, ta_gs, tb_ms, tb_gs])
 mode = input("Please set the mode for this game. Press enter to skip [interactive / testing]: ")
 mode = mode if mode != "" else 'interactive'
 
-data_file = input("Choose the data file for this game. If no preference, press enter to skip [1. TODO / 2. TODO 3./ TODO]: ")
+data_file = input("Choose the data file for this game. If no preference, press enter to skip [1. GloVe / 2. Word2Vec 3./ WordNet]: ")
 data_file = int(data_file) if data_file != "" else 1
 
 seed = input("Please enter the random seed for this game. If no preference, press enter to skip: ")
